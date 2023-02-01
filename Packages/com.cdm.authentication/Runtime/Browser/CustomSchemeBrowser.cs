614e48565837e135cae7d79d0ce43bf94813fdf4
Manuel Espino <manuel.espinolara@materialise.com>
2023-02-01 12:23:19 +0100

    Add CustomSchemeBrowser

---
+++ Packages/com.cdm.authentication/Runtime/Browser/CustomSchemeBrowser.cs
@@ -0,0 +1,43 @@
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Cdm.Authentication.Browser
+{
+    /// <summary>
+    /// OAuth 2.0 verification browser that runs a local server and waits for a call with
+    /// the authorization verification code throught a custom scheme instead of a http listener
+    /// </summary>
+    public class CustomSchemeBrowser : IBrowser
+    {
+        private TaskCompletionSource<BrowserResult> _taskCompletionSource;
+
+        public async Task<BrowserResult> StartAsync(string loginUrl, string redirectUrl, CancellationToken cancellationToken = default)
+        {
+            _taskCompletionSource = new TaskCompletionSource<BrowserResult>();
+
+            cancellationToken.Register(() =>
+            {
+                _taskCompletionSource?.TrySetCanceled();
+            });
+
+            Application.deepLinkActivated += onDeepLinkActivated;
+
+            try
+            {
+                Application.OpenURL(loginUrl);
+                return await _taskCompletionSource.Task;
+            }
+            finally
+            {
+                Application.deepLinkActivated -= onDeepLinkActivated;
+            }
+        }
+
+        private void onDeepLinkActivated(string url)
+        {
+            _taskCompletionSource.SetResult(
+                new BrowserResult(BrowserStatus.Success, url));
+        }
+    }
+}
